9862126f4cf60e84ecfac2237e306a7dba3f1ca4
muiscript <myam0118@gmail.com>
2019-03-16 12:31:54 +0900

    divide git package into 2 files

--- git/repository.go
+++ git/repository.go
@@ -1,79 +1,14 @@
 package git
 
 import (
-	"bufio"
 	"errors"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"os"
 	"path"
-	"strconv"
-	"strings"
 )
 
 const defaultDescription = "Unnamed repository; edit this file 'description' to name the repository.\n"
-
-type Config struct {
-	RepositoryFormatVersion int
-	FileMode                bool
-	Bare                    bool
-}
-
-var DefaultConfig = Config{
-	RepositoryFormatVersion: 0,
-	FileMode:                true,
-	Bare:                    false,
-}
-
-func NewConfig(confFile io.Reader) (*Config, error) {
-	sc := bufio.NewScanner(confFile)
-
-	// default conf
-	conf := &DefaultConfig
-	for sc.Scan() {
-		line := strings.TrimSpace(sc.Text())
-		if strings.HasPrefix(line, "repositoryformatversion") {
-			elems := strings.Split(line, "=")
-			if len(elems) == 2 {
-				vs := strings.TrimSpace(elems[1])
-				v, err := strconv.Atoi(vs)
-				if err == nil {
-					conf.RepositoryFormatVersion = v
-				}
-			}
-		}
-		if strings.HasPrefix(line, "filemode") {
-			elems := strings.Split(line, "=")
-			if len(elems) == 2 {
-				ms := strings.TrimSpace(elems[1])
-				m, err := strconv.ParseBool(ms)
-				if err == nil {
-					conf.FileMode = m
-				}
-			}
-		}
-		if strings.HasPrefix(line, "bare") {
-			elems := strings.Split(line, "=")
-			if len(elems) == 2 {
-				bs := strings.TrimSpace(elems[1])
-				b, err := strconv.ParseBool(bs)
-				if err == nil {
-					conf.Bare = b
-				}
-			}
-		}
-	}
-
-	return conf, nil
-}
-
-func (c *Config) Format() string {
-	return fmt.Sprintf(`[core]
-	repositoryformatversion = %d
-	filemode = %t
-	bare = %t`, c.RepositoryFormatVersion, c.FileMode, c.Bare)
-}
 
 type Repository struct {
 	WorkTree string
@@ -133,7 +68,6 @@ func CreateRepository(workTree string) (*Repository, error) {
 	return repo, nil
 }
 
-// TODO: privateにする
 // force disables all check for initializing repository, which is used when 'git init'.
 func NewRepository(workTree string, force bool) (*Repository, error) {
 	gitDir := path.Join(workTree, ".git")
--- git/repository_test.go
+++ git/repository_test.go
@@ -2,56 +2,9 @@ package git
 
 import (
 	"github.com/stretchr/testify/assert"
-	"io"
 	"path"
-	"strings"
 	"testing"
 )
-
-func TestNewConfig(t *testing.T) {
-	tests := []struct {
-		name           string
-		confFile       io.Reader
-		expectedConfig *Config
-		expectedErrMsg string
-	}{
-		{
-			name:           "no setting",
-			confFile:       strings.NewReader(""),
-			expectedConfig: &DefaultConfig,
-		},
-		{
-			name: "custom setting",
-			confFile: strings.NewReader(`
-[core]
-        repositoryformatversion = 1
-        filemode = false
-        bare = true
-        logallrefupdates = true
-        ignorecase = true
-        precomposeunicode = true
-[remote "origin"]
-        url = git@github.com:muiscript/gog.git
-        fetch = +refs/heads/*:refs/remotes/origin/*
-`),
-			expectedConfig: &Config{
-				RepositoryFormatVersion: 1,
-				FileMode:                false,
-				Bare:                    true,
-			},
-		},
-	}
-
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			config, err := NewConfig(test.confFile)
-			assert.Equal(t, test.expectedConfig, config)
-			if test.expectedErrMsg != "" {
-				assert.EqualError(t, err, test.expectedErrMsg)
-			}
-		})
-	}
-}
 
 var nonExistentDirectory = "./git/testdata/nonexistent01"
 var notGitRepository = "./git/testdata/not_repository01"
